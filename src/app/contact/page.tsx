06773648e92c488c44aaf3441a5f052f0b62e340
dhirajlaha <lahadhiraj@gmail.com>
2024-03-01 01:56:04 +0530

    Added routes,footer and 3d-card

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="min-h-screen max-w-7xl bg-black py-12 pt-36">
+      <h1 className="text-lg md:text-7xl font-bold text-white font-sans text-center mb-8">
+        Contact Us
+      </h1>
+    </div>
+  );
+}
+
+export default Contact;
